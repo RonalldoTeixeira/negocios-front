85b77b31794f2608d55f3f671a4d928f4909a529
RonalldoTeixeira <ronalldo.tb@gmail.com>
2023-09-27 21:06:59 -0300

    ...

--- Js/app.js
+++ Js/app.js
@@ -4,7 +4,10 @@ $(document).ready(function () {
 
 var cardapio = {};
 
-var MEU_CARRINHO =[];
+var MEU_CARRINHO = [];
+
+var VALOR_CARRINHO = 0 ;
+var VALOR_ENTREGA = 5;
 
 cardapio.eventos = {
 
@@ -236,11 +239,17 @@ carregarCarrinho: () => {
         .replace(/\${qntd}/g, e.qntd)
 
         $("#itensCarrinho").append(temp);
+
+          if ((i + 1) == MEU_CARRINHO.length) {
+            cardapio.metodos.carregarValores();
+            
+          }       
     })
 
     
   } else {
     $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i>Seu carrinho está vazio</p>');
+    cardapio.metodos.carregarValores()
   }
   
 
@@ -291,7 +300,42 @@ atualizarCarrinho: (id, qntd) => {
 
   // Atualiza o botão carrinho com a quantidade atualizada
   cardapio.metodos.atualizarBadgeTotal();
+
+  cardapio.metodos.carregarValores()
 },
+
+//carrega os valores de SubTotal, entrega e total
+carregarValores: () => {
+  VALOR_CARRINHO = 0;
+
+  $("#lblSubTotal").text('R$ 0,00');
+  $("#lblValorEntrega").text('+ R$ 0,00');
+  $("#lblValorTotal").text('R$ 0,00');
+  $.each(MEU_CARRINHO, (i, e) => {
+    VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+      if ((i + 1) == MEU_CARRINHO.length) {
+
+        $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace('.', ',')}`);
+        $("#lblValorEntrega").text(`+ R$ ${VALOR_ENTREGA.toFixed(2).replace('.', ',')}`);
+        $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}`);
+      }
+
+  })
+
+
+},
+
+//carregar a etapa endereços
+carregarEndereco: () => {
+
+  if (condition) {
+    
+  }
+
+
+},
+
 
   //mensagens
   mensagem: (texto, cor ='red', tempo = 3500) => {
